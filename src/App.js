92c921c4d282fe3964f67d67a93e7a73718f608f
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2022-11-11 14:05:23 +0800

    add ref link

--- src/App.js
+++ src/App.js
@@ -17,7 +17,9 @@ export default App;
       https://youtu.be/NqzdVN2tyvQ
    2. React Redux Quick Start
       https://react-redux.js.org/tutorials/quick-start
-   3. Redux Fundamentals, Part 2: Concepts and Data Flow
+   3. Redux Fundamentals, Part 1: Redux Overview
+      https://redux.js.org/tutorials/fundamentals/part-1-overview
+   4. Redux Fundamentals, Part 2: Concepts and Data Flow
       https://redux.js.org/tutorials/fundamentals/part-2-concepts-data-flow
 
  */
