9b7409b492a11beddd3fb34f2242a14fe9858522
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2022-12-30 17:07:28 +0800

    add pokemonapp

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,29 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from '../features/counter/counterSlice'
 
+import { setupListeners } from '@reduxjs/toolkit/query'
+import { pokemonApi } from '../services/pokemon'
+
 // Create a new Redux store with the `createStore` function,
 // and use the `counterReducer` for the update logic
 export const store = configureStore({
     reducer: {
-        counter: counterReducer
-    }
+            counter: counterReducer,
 
+            // Add the generated reducer as a specific top-level slice
+            [pokemonApi.reducerPath]: pokemonApi.reducer,
+        },
+        // Adding the api middleware enables caching, invalidation, polling,
+        // and other useful features of `rtk-query`.
+        middleware: (getDefaultMiddleware) =>
+            getDefaultMiddleware().concat(pokemonApi.middleware),
+  })
+
+/*
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+    }
 })
+
+*/
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import PokemonApp from './PokemonApp';
 import { store } from './app/store'
 import { Provider } from 'react-redux'
 
@@ -9,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-    <App />
+    <PokemonApp />
     </Provider>  
   </React.StrictMode>
 );
