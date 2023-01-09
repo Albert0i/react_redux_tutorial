ac793fc5c3b8e84d2b7742693f821d774576acc8
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-01-09 17:28:58 +0800

    add yup ref

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -2,6 +2,30 @@ import React from 'react';
  import { useFormik } from 'formik';
  import * as Yup from 'yup';
  
+  const validate = values => {
+  const errors = {};
+
+  if (!values.firstName) {
+    errors.firstName = 'Required';
+  } else if (values.firstName.length > 15) {
+    errors.firstName = 'Must be 15 characters or less';
+  }
+
+  if (!values.lastName) {
+    errors.lastName = 'Required';
+  } else if (values.lastName.length > 20) {
+    errors.lastName = 'Must be 20 characters or less';
+  }
+
+  if (!values.email) {
+    errors.email = 'Required';
+  } else if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address';
+  }
+
+  return errors;
+};
+
  const SignupForm = () => {
    const formik = useFormik({
      initialValues: {
@@ -55,5 +79,8 @@ export default SignupForm;
 
 /*
     FORMIK Tutorial
+    https://formik.org/docs/tutorial
+
+    npm | yup 
     https://formik.org/docs/tutorial
 */
