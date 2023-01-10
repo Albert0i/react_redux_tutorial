60141e591e4c6428a69e6780a297628c2c9eebd6
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-01-10 10:30:40 +0800

    formating

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,52 +1,39 @@
 import React from 'react';
-import { useFormik } from 'formik';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import { validate, validationSchema } from './validations';
 import "./styles.css";
 
 const SignupForm = () => {
-  const formik = useFormik({
-    initialValues: {
-       firstName: '',
-       lastName: '',
-       email: '',
-    },
-
-    //validate,
-    validationSchema, 
-    onSubmit: values => {
-       alert(JSON.stringify(values, null, 2));
-    },
-  });
-  
   return (
     <section className="login">
-      <form onSubmit={formik.handleSubmit}>
-        <label htmlFor="firstName">First Name</label>
-        <input
-          id="firstName"
-          type="text"
-            {...formik.getFieldProps('firstName')}
-        />
-        {formik.touched.firstName && formik.errors.firstName ? (
-          <div className="errmsg">{formik.errors.firstName}</div>
-        ) : null}
-  
-        <label htmlFor="lastName">Last Name</label>
-        <input id="lastName" type="text" {...formik.getFieldProps('lastName')} />
-        {formik.touched.lastName && formik.errors.lastName ? (
-          <div className="errmsg">{formik.errors.lastName}</div>
-        ) : null}
-  
-        <label htmlFor="email">Email Address</label>
-        <input id="email" type="email" {...formik.getFieldProps('email')} />
-        {formik.touched.email && formik.errors.email ? (
-          <div className="errmsg">{formik.errors.email}</div>
-        ) : null}
-  
-        <button type="submit">Submit</button>
-      </form>
+      <Formik
+        initialValues={{ firstName: '', lastName: '', email: '' }}
+        validationSchema={ validationSchema }
+        onSubmit={(values, { setSubmitting }) => {
+          setTimeout(() => {
+            alert(JSON.stringify(values, null, 2));
+            setSubmitting(false);
+          }, 400);
+        }}
+      >
+        <Form>
+          <label htmlFor="firstName">First Name</label>
+          <Field name="firstName" type="text" />
+          <ErrorMessage name="firstName" />
+
+          <label htmlFor="lastName">Last Name</label>
+          <Field name="lastName" type="text" />
+          <ErrorMessage name="lastName" />
+
+          <label htmlFor="email">Email Address</label>
+          <Field name="email" type="email" />
+          <ErrorMessage name="email" />
+
+          <button type="submit">Submit</button>
+        </Form>
+      </Formik>
     </section>
-   );
+  );
 };
 
 export default SignupForm;
