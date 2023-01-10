3f7ebc98b8db8b9bec0b4f052c4c2eece6d1824f
unknown <albertoi@20143ALBERTOI.ih.gov.mo>
2023-01-10 12:12:39 +0800

    eom

--- src/components/SignupForm.js
+++ src/components/SignupForm.js
@@ -1,13 +1,65 @@
 import React from 'react';
-import { Formik, Form, Field, ErrorMessage } from 'formik';
+import { Formik, Form, useField } from 'formik';
 import { validate, validationSchema } from './validations';
 import "./styles.css";
+
+const MyTextInput = ({ label, ...props }) => {
+  // useField() returns [formik.getFieldProps(), formik.getFieldMeta()]
+  // which we can spread on <input>. We can use field meta to show an error
+  // message if the field is invalid and it has been touched (i.e. visited)
+  const [field, meta] = useField(props);
+  return (
+    <>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <input className="text-input" {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </>
+  );
+};
+
+const MyCheckbox = ({ children, ...props }) => {
+  // React treats radios and checkbox inputs differently other input types, select, and textarea.
+  // Formik does this too! When you specify `type` to useField(), it will
+  // return the correct bag of props for you -- a `checked` prop will be included
+  // in `field` alongside `name`, `value`, `onChange`, and `onBlur`
+  const [field, meta] = useField({ ...props, type: 'checkbox' });
+  return (
+    <div>      
+      <input type="checkbox" {...field} {...props} />
+      {children}      
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
+
+const MySelect = ({ label, ...props }) => {
+  const [field, meta] = useField(props);
+  return (
+    <div>
+      <label htmlFor={props.id || props.name}>{label}</label>
+      <select {...field} {...props} />
+      {meta.touched && meta.error ? (
+        <div className="error">{meta.error}</div>
+      ) : null}
+    </div>
+  );
+};
 
 const SignupForm = () => {
   return (
     <section className="login">
       <Formik
-        initialValues={{ firstName: '', lastName: '', email: '' }}
+        initialValues={{
+          firstName: '',
+          lastName: '',
+          email: '',
+          acceptedTerms: false, // added for our checkbox
+          jobType: '', // added for our select
+        }}
         validationSchema={ validationSchema }
         onSubmit={(values, { setSubmitting }) => {
           setTimeout(() => {
@@ -16,19 +68,40 @@ const SignupForm = () => {
           }, 400);
         }}
       >
-        <Form>
-          <label htmlFor="firstName">First Name</label>
-          <Field name="firstName" type="text" />
-          <ErrorMessage name="firstName" />
-
-          <label htmlFor="lastName">Last Name</label>
-          <Field name="lastName" type="text" />
-          <ErrorMessage name="lastName" />
-
-          <label htmlFor="email">Email Address</label>
-          <Field name="email" type="email" />
-          <ErrorMessage name="email" />
-
+        <Form>          
+          <MyTextInput
+             label="First Name"
+             name="firstName"
+             type="text"
+             placeholder="Jane"
+          />
+ 
+          <MyTextInput
+             label="Last Name"
+             name="lastName"
+             type="text"
+             placeholder="Doe"
+          />
+ 
+          <MyTextInput
+             label="Email Address"
+             name="email"
+             type="email"
+             placeholder="jane@formik.com"
+          />
+ 
+          <MySelect label="Job Type" name="jobType">
+             <option value="">Select a job type</option>
+             <option value="designer">Designer</option>
+             <option value="development">Developer</option>
+             <option value="product">Product Manager</option>
+             <option value="other">Other</option>
+          </MySelect>
+ 
+          <MyCheckbox name="acceptedTerms">
+             I accept the terms and conditions
+          </MyCheckbox>
+ 
           <button type="submit">Submit</button>
         </Form>
       </Formik>
